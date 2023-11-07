35943c57ea8ce14ce03dbaa62c3c8a759e1492d9
Juan Sueldo <78236717+juansueldo@users.noreply.github.com>
2023-11-07 11:55:14 +0000

    captcha mejoras

--- src/app/components/captcha/captcha.component.ts
+++ src/app/components/captcha/captcha.component.ts
@@ -16,6 +16,7 @@ export class CaptchaComponent {
   mensaje: string;
   disabledButton: boolean = false;
   colorText: string;
+  showCaptcha: boolean = false;
   constructor(private pokeapiService: PokeapiService){}
 
   ngOnInit() {
@@ -27,7 +28,7 @@ export class CaptchaComponent {
     this.pokeapiService.getRandomPokemon().subscribe(
       (response) => {
         const pokemonName = response.name[0].toUpperCase() + response.name.substring(1);
-        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${response.id}.png`/*response.sprites.front_default*/;
+        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${response.id}.png`;
         this.pokemonImage = imageUrl;
         this.pokemonNameCorrect =pokemonName.toUpperCase();
         this.generateOptions(pokemonName);
@@ -89,4 +90,66 @@ export class CaptchaComponent {
       this.getRandomPokemon();
     }
   }
+  toggleCaptcha() {
+    this.showCaptcha = !this.showCaptcha; // Cambia el estado del captcha
+  }/*
+  number1: number;
+  number2: number;
+  operator: string;
+  correctAnswer: number;
+  options: number[];
+
+  constructor() {
+    this.generateCaptcha();
+  }
+
+  generateCaptcha() {
+    this.number1 = Math.floor(Math.random() * 10); // Número aleatorio del 0 al 9
+    this.number2 = Math.floor(Math.random() * 10);
+    this.operator = Math.random() < 0.5 ? '+' : '-'; // Operador aleatorio: suma o resta
+
+    if (this.operator === '+') {
+      this.correctAnswer = this.number1 + this.number2;
+    } else {
+      this.correctAnswer = this.number1 - this.number2;
+    }
+
+    // Genera opciones de respuesta aleatorias
+    this.options = this.generateOptions(this.correctAnswer, 4); // Genera 4 opciones incluyendo la correcta
+  }
+
+  generateOptions(correctAnswer: number, numOptions: number): number[] {
+    const options: number[] = [correctAnswer];
+
+    while (options.length < numOptions) {
+      const randomOption = this.generateRandomOption(correctAnswer);
+      if (!options.includes(randomOption)) {
+        options.push(randomOption);
+      }
+    }
+
+    return this.shuffleArray(options);
+  }
+
+  generateRandomOption(correctAnswer: number): number {
+    // Genera un valor aleatorio dentro de un rango que no sea igual al valor correcto
+    let randomOption = correctAnswer;
+    while (randomOption === correctAnswer) {
+      randomOption = Math.floor(Math.random() * 20); // Puedes ajustar el rango según tus necesidades
+    }
+    return randomOption;
+  }
+
+  shuffleArray(array: number[]): number[] {
+    // Baraja el array de opciones
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  checkAnswer(selectedAnswer: number): boolean {
+    return selectedAnswer === this.correctAnswer;
+  }*/
 }
