937cbaaa2d4d83bb76949d161bd50e3c7bba38b4
Juan Sueldo <78236717+juansueldo@users.noreply.github.com>
2023-11-29 19:27:27 +0000

    correcciones colores

--- src/app/pages/estadisticas/estadisticas.component.ts
+++ src/app/pages/estadisticas/estadisticas.component.ts
@@ -36,13 +36,13 @@ export class EstadisticasComponent {
   especialidades:string[] = [];
   colores:string[] = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
   backgroundColor: string[] =[
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(201, 203, 207, 0.2)'
+    'rgba(255, 99, 132, 0.5)',
+    'rgba(255, 159, 64, 0.5)',
+    'rgba(255, 205, 86, 0.5)',
+    'rgba(75, 192, 192, 0.5)',
+    'rgba(54, 162, 235, 0.5)',
+    'rgba(153, 102, 255, 0.5)',
+    'rgba(201, 203, 207, 0.5)'
   ];
   borderColor:string[] = [
     'rgb(255, 99, 132)',
